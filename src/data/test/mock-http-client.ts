75f4e9b28a389aa90190fd3d484ddc605c61fa93
italofelipe <felipe.italo@hotmail.com>
2021-09-21 09:20:01 -0300

    refactor: grouped imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,14 +1,21 @@
-import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http.response'
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+  HttpStatusCode
+} from '@/data/protocols/http'
 
-export class HttpPostClientSpy<BodyType, Response> implements HttpPostClient<BodyType, Response> {
+export class HttpPostClientSpy<BodyType, Response>
+implements HttpPostClient<BodyType, Response> {
   url?: string
   body?: BodyType
   response: HttpResponse<Response> = {
     statusCode: HttpStatusCode.ok
   }
 
-  async post (params: HttpPostParams<BodyType>): Promise<HttpResponse<Response>> {
+  async post (
+    params: HttpPostParams<BodyType>
+  ): Promise<HttpResponse<Response>> {
     this.url = params.url
     this.body = params.body
     return await Promise.resolve(this.response)
--- src/data/usecases/authentication/remote-authentication.test.ts
+++ src/data/usecases/authentication/remote-authentication.test.ts
@@ -1,10 +1,9 @@
-import { HttpStatusCode } from '@/data/protocols/http/http.response'
-import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
-import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { HttpPostClientSpy } from '@/data/test'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models/account-model'
-import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { mockAccountModel, mockAuthentication } from '@/domain/test'
+import { AuthenticationParams } from '@/domain/usecases'
 import faker from 'faker'
 import { RemoteAuthentication } from './remote-authentication'
 
@@ -14,7 +13,10 @@ type SutTypes = {
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
+  const httpPostClientSpy = new HttpPostClientSpy<
+  AuthenticationParams,
+  AccountModel
+  >()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
     sut,
